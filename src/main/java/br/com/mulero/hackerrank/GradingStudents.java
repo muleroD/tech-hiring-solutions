ceccedceb9040ea99cf760d66ba2a7b4650372b4
Richard Mulero <mulerolp@gmail.com>
2024-08-02 00:18:09 -0300

    feat: criando desafio GradingStudents.java e respectivo teste

---
+++ src/main/java/br/com/mulero/hackerrank/GradingStudents.java
@@ -0,0 +1,72 @@
+package br.com.mulero.hackerrank;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+/**
+ * <a href="https://www.hackerrank.com/challenges/grading/problem">Grading Students</a>
+ * <p>
+ * Dado um array de notas, arredonde as notas de acordo com as regras:
+ * <p>
+ * - Se a diferença entre a nota e o próximo múltiplo de 5 for menor que 3, arredonde a nota para o próximo múltiplo de 5.
+ * <p>
+ * - Se a nota for menor que 38, não arredonde a nota.
+ */
+public class GradingStudents {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int gradesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> grades = IntStream.range(0, gradesCount).mapToObj(i -> {
+                    try {
+                        return bufferedReader.readLine().replaceAll("\\s+$", "");
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                })
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = gradingStudents(grades);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining("\n"))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+
+    public static List<Integer> gradingStudents(List<Integer> grades) {
+        List<Integer> roundedGrades = new ArrayList<>();
+
+        for (Integer grade : grades) {
+            if (grade < 38) roundedGrades.add(grade);
+            else {
+                for (int i = 0; i < 3; i++) {
+                    int up = grade + i;
+                    if (grade % 5 == 0 || up % 5 == 0) {
+                        roundedGrades.add(up);
+                        break;
+                    } else if (i == 2) {
+                        roundedGrades.add(grade);
+                    }
+                }
+            }
+        }
+
+        return roundedGrades;
+    }
+}
---
+++ src/test/java/br/com/mulero/hackerrank/GradingStudentsTest.java
@@ -0,0 +1,78 @@
+package br.com.mulero.hackerrank;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static br.com.mulero.hackerrank.GradingStudents.gradingStudents;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class GradingStudentsTest {
+
+    @Test
+    void givenSimpleTestFromHackerRank_thenRound() {
+        // Given
+        List<Integer> grades = Arrays.asList(73, 67, 38, 33);
+        List<Integer> expected = Arrays.asList(75, 67, 40, 33);
+
+        // When
+        List<Integer> result = gradingStudents(grades);
+
+        // Then
+        assertArrayEquals(expected.toArray(), result.toArray());
+    }
+
+    @Test
+    void givenOnlyMultiplesofFiveLessThan38_thenDoNotRound() {
+        // Given
+        List<Integer> grades = Arrays.asList(5, 10, 15, 20, 25, 30, 35);
+        List<Integer> expected = Arrays.asList(5, 10, 15, 20, 25, 30, 35);
+
+        // When
+        List<Integer> result = gradingStudents(grades);
+
+        // Then
+        assertArrayEquals(expected.toArray(), result.toArray());
+    }
+
+    @Test
+    void givenGradesLessThan38_thenDoNotRound() {
+        // Given
+        List<Integer> grades = Arrays.asList(33, 36, 37);
+        List<Integer> expected = Arrays.asList(33, 36, 37);
+
+        // When
+        List<Integer> result = gradingStudents(grades);
+
+        // Then
+        assertArrayEquals(expected.toArray(), result.toArray());
+    }
+
+    @Test
+    void givenGradesGreaterThan38_thenRound() {
+        // Given
+        List<Integer> grades = Arrays.asList(38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49);
+        List<Integer> expected = Arrays.asList(40, 40, 40, 41, 42, 45, 45, 45, 46, 47, 50, 50);
+
+        // When
+        List<Integer> result = gradingStudents(grades);
+
+        // Then
+        assertArrayEquals(expected.toArray(), result.toArray());
+    }
+
+    @Test
+    void givenGradesGreaterThan38_thenRound2() {
+        // Given
+        List<Integer> grades = Arrays.asList(73, 67, 38, 33, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49);
+        List<Integer> expected = Arrays.asList(75, 67, 40, 33, 40, 40, 41, 42, 45, 45, 45, 46, 47, 50, 50);
+
+        // When
+        List<Integer> result = gradingStudents(grades);
+
+        // Then
+        assertArrayEquals(expected.toArray(), result.toArray());
+    }
+
+}
