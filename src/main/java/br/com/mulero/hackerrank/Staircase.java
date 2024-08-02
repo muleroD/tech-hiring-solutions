5b952a0b82a2c7cacf5db2b9ad9c4aa43b44b3ba
Richard Mulero <richard.mulero@fcamara.com.br>
2024-08-02 18:07:16 -0300

    feat: adicionando desafio Staircase

---
+++ src/main/java/br/com/mulero/hackerrank/Staircase.java
@@ -0,0 +1,36 @@
+package br.com.mulero.hackerrank;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * <a href="https://www.hackerrank.com/challenges/staircase/problem">Staircase</a>
+ * <p>
+ * Dado uma escada de tamanho n, imprime uma escada de tamanho n com # e espaços.
+ */
+public class Staircase {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        staircase(n);
+
+        bufferedReader.close();
+    }
+
+    private static void staircase(int n) {
+        List<String> stair = new ArrayList<>(Collections.nCopies(n, " "));
+
+        for (int i = n - 1; i >= 0; i--) {
+            stair.set(i, "#");
+            // Melhorar para remover o reduce
+            System.out.println(stair.stream().reduce((s, s2) -> s + s2).orElse(""));
+        }
+    }
+}
